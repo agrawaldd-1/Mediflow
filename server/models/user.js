b21cad27eeabb0bdc8a45974e620768bd76f9754
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-07-17 17:18:54 +0530

    create User model with role-based schema

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true,
+        trim : true
+    },
+    email : {
+        type : String,
+        required : true,
+        trim : true,
+        unique : true,
+        lowercase : true
+    },
+    password:{
+        type : String,
+        required : true,
+        minlength: 8
+    },
+    role : {
+        type : String,
+        required : true,
+        enum: ["patient", "doctor", "receptionist"]
+    },
+    isActive : {
+        type : Boolean,
+        default : true
+    },
+},
+{
+    timestamps: true,
+})
+
+export const User = mongoose.model("User" , userSchema);
