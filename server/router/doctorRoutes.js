dba5bbfc37bca6b1f048b19b3656135405c1eebd
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-07-26 11:46:13 +0530

    Doctor  Module all bugs removed and polished code

--- server/router/doctorRoutes.js
+++ server/router/doctorRoutes.js
@@ -1,16 +1,57 @@
-import express from "express"
-import { authorize, protect } from "../Middleware/authMiddleware.js";
-import { getAllDoctors, getDoctorById, registerDoctor, searchDoctor , updateDoctorProfile , updateDoctorStatus } from "../controller/doctorController.js";
+import express from "express";
+import {
+    getAllDoctors,
+    getDoctorById,
+    registerDoctor,
+    searchDoctor,
+    updateDoctorProfile,
+    updateDoctorStatus,
+} from "../controller/doctorController.js";
+
+import { protect, authorize } from "../Middleware/authMiddleware.js";
 
 const router = express.Router();
-router.post("/" , protect , authorize(["admin"]) , registerDoctor)
-router.get("/", protect , authorize(["admin", "receptionist"]) , getAllDoctors)
-router.get("/search" , protect , authorize(["admin", "receptionist"]) , searchDoctor)
-router.get("/:id" , protect , authorize(["admin", "receptionist"]) , getDoctorById)
-router.put("/:doctorId",protect,authorize(["admin"]),updateDoctorProfile);
 
+router.post(
+    "/",
+    protect,
+    authorize(["admin"]),
+    registerDoctor
+);
 
-router.patch("/:doctorId/status",protect,authorize(["admin"]),updateDoctorStatus);
+router.get(
+    "/",
+    protect,
+    authorize(["admin", "receptionist"]),
+    getAllDoctors
+);
 
+router.get(
+    "/search",
+    protect,
+    authorize(["admin", "receptionist"]),
+    searchDoctor
+);
+
+router.get(
+    "/:doctorId",
+    protect,
+    authorize(["admin", "receptionist"]),
+    getDoctorById
+);
+
+router.put(
+    "/:doctorId",
+    protect,
+    authorize(["admin"]),
+    updateDoctorProfile
+);
+
+router.patch(
+    "/:doctorId/status",
+    protect,
+    authorize(["admin"]),
+    updateDoctorStatus
+);
 
 export default router;
