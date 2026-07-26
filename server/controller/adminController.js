2e2f56f9dd05c08489070761ad9efa7bd8c84855
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-07-26 09:30:04 +0530

    Admin Module all bugs removed and polished code

--- server/controller/adminController.js
+++ server/controller/adminController.js
@@ -1,18 +1,19 @@
 import { Doctor } from "../models/doctors.js";
 import { Patient } from "../models/patients.js";
 import { Appointment } from "../models/appointments.js";
-import { User } from "../models/user.js";
-import { Receptionist } from "../models/receptionist.js";
+import { Invoice } from "../models/invoice.js";
 
 export const getAdminDashboard = async (req, res) => {
     try {
         const [
             doctorCountResult,
             patientCountResult,
+            revenueResult,
             totalBookedAppointments,
             totalCompletedAppointments,
             totalCanceledAppointments,
-            totalAppointments
+            totalAppointments,
+            pendingPayments,
         ] = await Promise.all([
             Doctor.aggregate([
                 {
@@ -57,28 +58,68 @@ export const getAdminDashboard = async (req, res) => {
                     $count: "totalPatients",
                 },
             ]),
-            Appointment.countDocuments({status : "Booked"}),
-            Appointment.countDocuments({status : "Completed"}),
-            Appointment.countDocuments({status : "Canceled"}),
-            Appointment.countDocuments()
+
+            Invoice.aggregate([
+                {
+                    $match: {
+                        paymentStatus: "Paid",
+                    },
+                },
+                {
+                    $group: {
+                        _id: null,
+                        totalRevenue: {
+                            $sum: "$totalAmount",
+                        },
+                    },
+                },
+            ]),
+
+            Appointment.countDocuments({
+                status: "Booked",
+            }),
+
+            Appointment.countDocuments({
+                status: "Completed",
+            }),
+
+            Appointment.countDocuments({
+                status: "Canceled",
+            }),
+
+            Appointment.countDocuments(),
+
+            Invoice.countDocuments({
+                paymentStatus: "Pending",
+            }),
         ]);
 
         const totalDoctors =
-            doctorCountResult.length > 0? doctorCountResult[0].totalDoctors: 0;
+            doctorCountResult.length > 0
+                ? doctorCountResult[0].totalDoctors
+                : 0;
 
-        const totalPatients =patientCountResult.length > 0?patientCountResult[0].totalPatients : 0;
+        const totalPatients =
+            patientCountResult.length > 0
+                ? patientCountResult[0].totalPatients
+                : 0;
 
-       
+        const totalRevenue =
+            revenueResult.length > 0
+                ? revenueResult[0].totalRevenue
+                : 0;
 
         return res.status(200).json({
             success: true,
             dashboard: {
                 totalDoctors,
                 totalPatients,
+                totalRevenue,
                 totalBookedAppointments,
                 totalCompletedAppointments,
                 totalCanceledAppointments,
                 totalAppointments,
+                pendingPayments,
             },
         });
     } catch (error) {
@@ -90,4 +131,3 @@ export const getAdminDashboard = async (req, res) => {
         });
     }
 };
-
--- server/router/adminRouter.js
+++ server/router/adminRouter.js
@@ -2,14 +2,13 @@ import express from "express";
 import { protect, authorize } from "../Middleware/authMiddleware.js";
 import { getAdminDashboard } from "../controller/adminController.js";
 
-
 const router = express.Router();
-router.get(
-  "/dashboard",
-  protect,
-  authorize(["admin"]),
-  getAdminDashboard
-);
 
+router.get(
+    "/dashboard",
+    protect,
+    authorize(["admin"]),
+    getAdminDashboard
+);
 
 export default router;
