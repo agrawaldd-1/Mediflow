8fb69d2479384c5f1d7c9695e4c118565a392c3f
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-02 08:37:04 +0530

    Add receptionist service APIs

--- src/services/receptionistService.js
+++ src/services/receptionistService.js
@@ -4,3 +4,61 @@ export const getReceptionistDashboard = async () => {
     const { data } = await api.get("/receptionist/dashboard");
     return data;
 };
+
+
+
+export const registerReceptionist = async (receptionistData) => {
+    const { data } = await api.post(
+        "/receptionist/register",
+        receptionistData
+    );
+
+    return data;
+};
+
+export const searchReceptionist = async (query) => {
+    const { data } = await api.get(
+        "/receptionist/search",
+        {
+            params: {
+                name : query,
+            },
+        }
+    );
+
+    return data;
+};
+
+export const updateReceptionistStatus = async (
+    receptionistId,
+    isActive
+) => {
+    const { data } = await api.patch(
+        `/receptionist/${receptionistId}/status`,
+        {
+            isActive,
+        }
+    );
+
+    return data;
+};
+
+export const getReceptionistById = async (receptionistId) => {
+    const { data } = await api.get(
+        `/receptionist/${receptionistId}`
+    );
+
+    return data;
+};
+
+export const updateReceptionist = async (
+    receptionistId,
+    receptionistData
+) => {
+    const { data } = await api.put(
+        `/receptionist/${receptionistId}`,
+        receptionistData
+    );
+
+    return data;
+};
