23c88b9402c0f0c75b28ef89d0052ce718ac4cb4
agrawaldd-1 <darshanagrawal04052005@gmail.com>
2026-08-02 08:36:58 +0530

    Add doctor service APIs

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -4,3 +4,48 @@ export const getTodaysAppointment = async()=>{
     const {data} = await api.get("appointments/today")
     return data;
 }
+
+export const registerDoctor = async (doctorData) => {
+    const { data } = await api.post("/doctors/", doctorData);
+    return data;
+};
+
+export const searchDoctor = async (query) => {
+    const { data } = await api.get("doctors/search", {
+        params: {
+            query,
+        },
+    });
+
+    return data;
+};
+
+export const updateDoctorProfile = async (doctorId, doctorData) => {
+    const { data } = await api.put(
+        `/doctors/${doctorId}`,
+        doctorData
+    );
+
+    return data;
+};
+
+export const getDoctorById = async (doctorId) => {
+    const { data } = await api.get(`/doctors/${doctorId}`);
+    return data;
+};
+
+export const updateDoctorStatus = async (doctorId, isActive) => {
+    const { data } = await api.patch(
+        `/doctors/${doctorId}/status`,
+        {
+            isActive,
+        }
+    );
+
+    return data;
+};
+
+export const getAllDoctors = async () => {
+    const { data } = await api.get("/doctors");
+    return data;
+};
